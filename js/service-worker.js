202ba3fef94ef69e8c970108eaa7c377256bd850
Matthew Cranford <mcranfordmail@gmail.com>
2018-08-07 09:43:21 -0600

    Add cache files

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,6 +1,12 @@
 const cacheName = 'v1';
 const cacheFiles = [
-
+    './',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js'
 ];
 
 self.addEventListener('install', function(event) {
