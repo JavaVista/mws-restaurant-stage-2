35f4dfd57c2825a5b5645c5807fd7b3e038ce508
Matthew Cranford <mcranfordmail@gmail.com>
2018-08-08 09:16:16 -0600

    Add to urls to cache

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,24 +1,30 @@
 const cacheName = 'v1';
 const cacheFiles = [
-    './',
-    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js'
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js'
 ];
 
-self.addEventListener('install', function(event) {
-    console.log('installed!');
+self.addEventListener('install', function(e) {
+    console.log('Installed');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('Caching cacheFiles');
+            return cache.addAll(cacheFiles);
+        })
+    )
 });
 
-self.addEventListener('activate', function(event) {
-    console.log('activated!');
+self.addEventListener('activate', function(e) {
+    console.log('Activated');
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log('fetching', event.request.url);
+self.addEventListener('fetch', function(e) {
+    console.log('Fetching', e.request);
 });
 
 
