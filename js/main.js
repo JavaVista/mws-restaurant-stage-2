a86f8b5d83b36da4d040503436699d3605daf19d
Matthew Cranford <mcranfordmail@gmail.com>
2018-08-14 08:27:18 -0600

    Remove comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
   .register('/service-worker.js')
-  .then(function(registration) {
-    console.log('[ServiceWorker] Registered', registration);
-  })
   .catch(function(err) {
-    console.log('[ServiceWorker] Registration error:', err);
+    console.error(err);
   });
 }
 
--- service-worker.js
+++ service-worker.js
@@ -23,45 +23,33 @@ const cacheFiles = [
 ];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching cacheFiles');
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
-});
-
 self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetching', e.request);
 
     e.respondWith(
         caches.match(e.request).then(function(response) {
 
             if (response) {
-                console.log('[ServiceWorker] Found in cache', e.request.url);
                 return response;
             }
 
             else {
-                console.log('[ServiceWorker] Not found in cache, fetching...',e.request);
-                const requestClone = e.request.clone();
-                
                 return fetch(e.request)
                     .then(function(response) {
                         const responseClone = response.clone();
                         caches.open(cacheName).then(function(cache) {
-                            console.log('[ServiceWorker] Storing in cache:', responseClone);
                             cache.put(e.request, responseClone);
                         })
                         return response;
                     })
-                    .catch(function() {
-                        console.log('[ServiceWorker] Error with fetch');
+                    .catch(function(err) {
+                        console.error(err);
                     });
             }         
         })
